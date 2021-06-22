f8cdf1707d789523e72b48b822264bec5baa53e6
starfish <zhangshaoyuanzsy@163.com>
2021-06-22 03:23:43 +0000

    修复CVE-2020-22025漏洞处理

--- ffmpeg-y/libavfilter/vf_edgedetect.c
+++ ffmpeg-y/libavfilter/vf_edgedetect.c
@@ -150,7 +150,10 @@ static void gaussian_blur(AVFilterContext *ctx, int w, int h,
     int i, j;
 
     memcpy(dst, src, w); dst += dst_linesize; src += src_linesize;
-    memcpy(dst, src, w); dst += dst_linesize; src += src_linesize;
+    // mod begin: fix CVE-2020-22025
+    if (h > 1)
+        memcpy(dst, src, w); dst += dst_linesize; src += src_linesize;
+    // mod end
     for (j = 2; j < h - 2; j++) {
         dst[0] = src[0];
         dst[1] = src[1];
@@ -180,8 +183,12 @@ static void gaussian_blur(AVFilterContext *ctx, int w, int h,
         dst += dst_linesize;
         src += src_linesize;
     }
-    memcpy(dst, src, w); dst += dst_linesize; src += src_linesize;
-    memcpy(dst, src, w);
+    // mod begin: fix CVE-2020-22025
+    if (h > 2)
+        memcpy(dst, src, w); dst += dst_linesize; src += src_linesize;
+    if (h > 3)
+        memcpy(dst, src, w);
+    // mod end
 }
 
 enum {
