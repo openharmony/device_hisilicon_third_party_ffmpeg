993e3bb08e022998eaeebe7ac91e84a5ca2f5ae7
unknown <shaoyuan.zhang@huawei.com>
2022-07-13 20:43:02 +0800

    [CVE-2022-1475] fix signed integer overflow
    Fixes: signed integer overflow: 10 * 808464428 cannot be represented in type 'int'
    Fixes: assertion failure
    Fixes: ticket9651
    
    Signed-off-by: starfish002 <shaoyuan.zhang@huawei.com>

--- ffmpeg-y/libavcodec/g729_parser.c
+++ ffmpeg-y/libavcodec/g729_parser.c
@@ -49,6 +49,10 @@ static int g729_parse(AVCodecParserContext *s1, AVCodecContext *avctx,
         s->block_size = (avctx->bit_rate < 8000) ? G729D_6K4_BLOCK_SIZE : G729_8K_BLOCK_SIZE;
         if (avctx->codec_id == AV_CODEC_ID_ACELP_KELVIN)
             s->block_size++;
+        // fix cve-2022-1475
+        // channels > 2 is invalid, we pass the packet on unchanged
+        if (avctx->channels > 2)
+            s->block_size = 0;
         s->block_size *= avctx->channels;
         s->duration   = avctx->frame_size;
     }
